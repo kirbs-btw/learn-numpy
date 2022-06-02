a73bf8f6a363bbf3f3551cbe04fc0bec9acf7619
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-02 14:00:55 +0200

    Add files via upload

---
+++ 2022-06-02/2022-06-02-a.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+"""
+defining my own unfunc
+
+hard codes it in numpy... i'm scared :)
+    
+"""
+
+
+def test_function():
+    pass
+
+def main():
+    # function = np.frompyfunc(test_function, 0, 0)
+
+
+
+
+if __name__ == '__main__':
+    main()
